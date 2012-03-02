5b0ea23740f399ebed8cc4556c6a37b5dd927b9a
ryonext <ryo.next@gmail.com>
2012-03-02 23:57:57 +0900

    twitter-login done

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,13 +8,14 @@ class LoginController < ApplicationController
 
     #DBにデータはある？
     user = User.find_by_twitter_id(auth["uid"])
-    p 1
     if user == nil
       #無いなら登録
       user = User.create_with_omniauth(auth)
     end
     #ユーザーIDをセッションに入れる
     session[:user_id] = user.id
+    #最終ログイン日もセッションに入れる
+    session[:last_login] = user.last_login
     #ログインしたあとのページヘ
     redirect_to :controller=>'service', :action=>'index'
   end
--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,5 +1,11 @@
 class ServiceController < ApplicationController
   def index
+    #画面に出すためのユーザー情報を取得
+    @user = User.find(session[:user_id])
+    #最終ログイン日更新（画面にはセッションの値を使う。）
+    @user.update_attribute(:last_login, Time.now)
+    #画面に出す最終ログイン日（セッションの値）
+    @last_login = session[:last_login]
   end
 
 end
